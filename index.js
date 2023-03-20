512c9cda07a16113c6becfa6e68e8bd68ecdb738
ahmed025566 <ahmedsalah025566@gmail.com>
2023-03-20 20:54:52 +0200

    Upload my scanner

---
+++ index.js
@@ -0,0 +1,36 @@
+const Percentage = document.querySelector(".Percentage");
+const volume = document.querySelector(".volume");
+const get = document.querySelector(".get");
+function displayPercenteage() {
+  dt1 = new Date("March 20, 2023 15:30:00");
+  dt2 = new Date();
+  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= 60;
+  const tim = Math.abs(Math.round(diff));
+  const checkPercentage = (tim * (1 / 60) + 5).toFixed(2);
+
+  return checkPercentage;
+}
+function displayVolume() {
+  dt1 = new Date("March 20, 2023 15:30:00");
+  dt2 = new Date();
+  var diff = (dt2.getTime() - dt1.getTime()) / 1000;
+  diff /= 60;
+  const tim = Math.abs(Math.round(diff));
+
+  const checkVolume = (tim * (1000000 / 390)).toFixed(0);
+  return checkVolume;
+}
+const resP = displayPercenteage();
+const resV = displayVolume();
+
+get.onclick = () => {
+  Percentage.innerHTML = "";
+  const per = document.createElement("p");
+  per.append(document.createTextNode("Requried Percentage is " + resP + "%"));
+  Percentage.append(per);
+  volume.innerHTML = '';
+  const v = document.createElement('p');
+  v.append(document.createTextNode("Requried Volume is " + resV + "$$"))
+  volume.append(v)
+};
